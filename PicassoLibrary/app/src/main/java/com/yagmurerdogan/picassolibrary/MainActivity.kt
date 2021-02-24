d02b6e9f1e659aa58b14e2a1810a1becdd235d84
Yagmur Erdogan <yagmur.erdogan.1999@gmail.com>
2021-02-24 15:49:11 +0300

    Attach files with view binding

--- PicassoLibrary/app/src/main/java/com/yagmurerdogan/picassolibrary/MainActivity.kt
+++ PicassoLibrary/app/src/main/java/com/yagmurerdogan/picassolibrary/MainActivity.kt
@@ -2,10 +2,15 @@ package com.yagmurerdogan.picassolibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yagmurerdogan.picassolibrary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val mViewBinding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mViewBinding.root)
+
+
     }
 }
